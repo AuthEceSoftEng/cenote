c211e85cd84ddff343ec903a905ce07fd0fc9978
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-03-26 14:21:41 +0200

    Add more tests (#10)

--- __test__/write.test.js
+++ __test__/write.test.js
@@ -10,7 +10,7 @@ describe('Test writing functionality', () => {
     expect(response.statusCode).toBe(204);
   }, 10000);
 
-  test('1000 new measurements should be written at most after 20 seconds.', async () => {
+  test('1000 new measurements should be written at most after 30 seconds.', async () => {
     const payload = [];
     for (let i = 1; i < 1001; i += 1) payload.push({ data: { a: i, b: i, c: i.toString() } });
     const body = { payload };
@@ -18,8 +18,24 @@ describe('Test writing functionality', () => {
     expect(response.statusCode).toBe(202);
     expect(response.body.length).toBe(1000);
     const query = { masterKey: CENOTE_MASTER_KEY, event_collection: 'test' };
-    await delay(20000);
+    await delay(30000);
     const { count } = (await got.get(`/projects/${PROJECT_ID}/queries/count`, { query })).body.results[0];
     expect(count).toBe(1000);
-  }, 22000);
+  }, 40000);
+
+  test('Writing to event collection with special characters in its name fails', async () => {
+    const payload = [{ data: { a: 0, b: 0, c: (0).toString() } }];
+    const body = { payload };
+    const response = await got.post(`/projects/${PROJECT_ID}/events/t3St?masterKey=${CENOTE_MASTER_KEY}`, { body });
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe('InvalidCollectionNameError');
+  });
+
+  test('Writing events with special characters in their keys fails', async () => {
+    const payload = [{ data: { a: 0, b: 0, c: { d: { e: { nApo: '66' } } } } }];
+    const body = { payload };
+    const response = await got.post(`/projects/${PROJECT_ID}/events/test?masterKey=${CENOTE_MASTER_KEY}`, { body });
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe('InvalidPropertyNameError');
+  });
 });

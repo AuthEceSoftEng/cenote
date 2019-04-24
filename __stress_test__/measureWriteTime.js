3e92b9b8fccc867c4c09c8a3afe774fca04ff727
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-04-24 18:15:01 +0300

    Closes #11

--- __stress_test__/measureWriteTime.js
+++ __stress_test__/measureWriteTime.js
@@ -1,14 +1,25 @@
 /* eslint-disable no-await-in-loop, no-restricted-syntax, no-bitwise */
-const got = require('got').extend({ baseUrl: process.env.CENOTE_API_URL, json: true, throwHttpErrors: false, agent: undefined });
+require('dotenv').config();
+const got = require('got').extend({ json: true, throwHttpErrors: false, agent: undefined });
 const delay = require('delay');
+const Chance = require('chance');
 
 const { PROJECT_ID, CENOTE_MASTER_KEY } = process.env;
+const POOL_SIZE = parseInt(process.env.POOL_SIZE, 10) || 3;
+const chance = new Chance(666);
+const servers = chance.pickset([
+  process.env.CENOTE_SERVER_1,
+  process.env.CENOTE_SERVER_2,
+  process.env.CENOTE_SERVER_3,
+], POOL_SIZE);
+
+const getRandomServer = () => chance.pickone(servers);
 
 module.exports = async (eventCollection, totalEvents) => {
   const startTime = Date.now();
   const query = { masterKey: CENOTE_MASTER_KEY, event_collection: eventCollection };
   for (const _ of Array(totalEvents).keys()) { // eslint-disable-line no-unused-vars
-    const { count } = (await got.get(`/projects/${PROJECT_ID}/queries/count`, { query })).body.results[0];
+    const { count } = (await got.get(`/projects/${PROJECT_ID}/queries/count`, { query, baseUrl: getRandomServer() })).body.results[0];
     if (count >= totalEvents) {
       const totalTime = (Date.now() - startTime) / 1000;
       return totalTime;
--- __stress_test__/sendBatch.js
+++ __stress_test__/sendBatch.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-await-in-loop, no-restricted-syntax, no-bitwise */
 require('dotenv').config();
-const got = require('got').extend({ baseUrl: process.env.CENOTE_API_URL, json: true, throwHttpErrors: false, agent: undefined });
+const got = require('got').extend({ json: true, throwHttpErrors: false, agent: undefined });
+const Chance = require('chance');
 
 const measureWriteTime = require('./measureWriteTime');
 
@@ -8,6 +9,7 @@ const { PROJECT_ID, CENOTE_MASTER_KEY } = process.env;
 
 const WRITES_PER_BATCH = parseInt(process.env.WRITES_PER_BATCH, 10) || 100;
 const TOTAL_EVENTS = 5e4;
+const POOL_SIZE = parseInt(process.env.POOL_SIZE, 10) || 3;
 
 const numOfRequests = ~~(TOTAL_EVENTS / WRITES_PER_BATCH);
 
@@ -16,15 +18,24 @@ const getRandomEvent = () => {
   return { data: { a: i, b: i ** 2, c: i.toString() } };
 };
 
+const chance = new Chance(666);
+const servers = chance.pickset([
+  process.env.CENOTE_SERVER_1,
+  process.env.CENOTE_SERVER_2,
+  process.env.CENOTE_SERVER_3,
+], POOL_SIZE);
+
+const getRandomServer = () => chance.pickone(servers);
+
 const sendToCenote = async () => {
   const times = [];
   const query = { masterKey: CENOTE_MASTER_KEY, event_collection: 'stresstest' };
-  const { count } = (await got.get(`/projects/${PROJECT_ID}/queries/count`, { query })).body.results[0];
+  const { count } = (await got.get(`/projects/${PROJECT_ID}/queries/count`, { query, baseUrl: getRandomServer() })).body.results[0];
   for (const i of Array(numOfRequests).keys()) {
     const eventPromises = [];
     for (let j = 0; j < WRITES_PER_BATCH; j += 1) {
       const body = { payload: getRandomEvent() };
-      eventPromises.push(got.post(`/projects/${PROJECT_ID}/events/stresstest?masterKey=${CENOTE_MASTER_KEY}`, { body }));
+      eventPromises.push(got.post(`/projects/${PROJECT_ID}/events/stresstest?masterKey=${CENOTE_MASTER_KEY}`, { body, baseUrl: getRandomServer() }));
     }
     const res = await Promise.all([measureWriteTime('stresstest', count + (i + 1) * WRITES_PER_BATCH), ...eventPromises]);
     times.push(res[0]);
@@ -35,10 +46,9 @@ const sendToCenote = async () => {
 (async () => {
   try {
     const times = await sendToCenote();
-    console.log(`Took an average of ${(times.reduce((prev, cur) => prev + cur, 0) / times.length).toFixed(2)} seconds to write ${
-      WRITES_PER_BATCH}/sec events.\n`);
     console.log(`Time per batch: ${times}`);
+    console.log(`Average response (batch) response time with ${POOL_SIZE} servers: ${times.reduce((a, b) => a + b, 0) / times.length}`);
   } catch (error) {
     console.error(error);
   }
-})().then(() => console.log('Done!'));
+})().then(() => console.log('Done!\n'));

7eb3aeef0ed292fed6c582f28250610e55aeb296
Giannis Antoniadis <johneegr@gmail.com>
2020-08-28 16:22:28 +0300

    Changed afterAll to beforeAll for testing experiments #73

--- __test__/setup.js
+++ __test__/setup.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
+
 const got = require("got").extend({ baseUrl: process.env.CENOTE_API_URL, json: true, throwHttpErrors: false });
 
 global.NUM_OF_DOCS = parseInt(process.env.NUM_OF_DOCS, 10) || 100;
 
 const { PROJECT_ID } = process.env;
-
-afterAll(async () => {
+beforeAll(async () => {
 	const response = await got.delete(`/projects/${PROJECT_ID}/queries/testCleanup`);
 	if (response.statusCode === 400) {
 		expect(response.body.ok).toBe(false);
